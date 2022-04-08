3e277662f138c3ba9f46d8378695688f28f4c82b
Melvin <contactmelvinadu@gmail.com>
2022-04-08 03:10:36 -0400

    add ability to only show tip form when user has a browser based wallet

--- tip.js
+++ tip.js
@@ -1,8 +1,11 @@
 const form = document.querySelector("form");
 
+if (window.ethereum) {
+    form.classList.add("has-eth");
+}
+
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     form.style.backgroundColor = 'red';
-
 });
